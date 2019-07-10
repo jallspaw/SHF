68d85349b77eabc9ca5d7ec9d844a3a29f8f8f1c
Daniel Schauenberg <mrtazz@github.com>
2019-07-10 18:57:50 -0500

    Make redirect 302
    as the redirect is randomly chosen it might be useful to not have it be cached forever by clients because of the permanent redirect but make them always request the original URL to potentially get a new redirect

--- web/index.php
+++ web/index.php
@@ -2,6 +2,6 @@
 $urls = array('https://www.homelessperiodproject.org/',"https://www.helpingwomenperiod.org/","https://isupportthegirls.org/"); //specify array of possible URLs
 $rand = rand(0,count($urls)-1); //get random number between 0 and array length
 $location = $urls[$rand]; //get random item from array
-header ('HTTP/1.1 301 Moved Permanently'); //send header
+header ('HTTP/1.1 302 Found'); //send header
 header ('Location: '.$location);
 ?>
