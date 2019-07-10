aee5cf910211c4a9cfe8dcb5912db241c405e9df
John Allspaw <allspaw@gmail.com>
2019-07-10 17:42:00 -0400

    randomly redirect to a site from a list of URLs

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$urls = array('https://www.homelessperiodproject.org/',"https://www.helpingwomenperiod.org/","https://isupportthegirls.org/"); //specify array of possible URLs
+$rand = rand(0,count($urls)-1); //get random number between 0 and array length
+$location = $urls[$rand]; //get random item from array
+header ('HTTP/1.1 301 Moved Permanently'); //send header
+header ('Location: '.$location);
+?>
